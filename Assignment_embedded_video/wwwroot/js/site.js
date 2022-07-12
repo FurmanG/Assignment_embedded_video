145b4271d76f1d90b1d2472fb8ec8be8e6f4c41c
Gil Furman <gilfurman88@gmail.com>
2022-07-12 15:29:35 -0400

    resubmit with animation

--- Assignment_embedded_video/wwwroot/js/site.js
+++ Assignment_embedded_video/wwwroot/js/site.js
@@ -12,3 +12,21 @@ function newColor() {
 function oldColor() {
     document.getElementById("title").style.color = "goldenrod";
 }
+
+
+function MoveTheBox() {
+    let id = null;
+    const elem = document.getElementById("animateBox");
+    let pos = 0;
+    clearInterval(id);
+    id = setInterval(frame, 5);
+    function frame() {
+        if (pos == 350) {
+            clearInterval(id);
+        } else {
+            pos++;
+            elem.style.top = pos + "px";
+            elem.style.left = pos + "px";
+        }
+    }
+}
