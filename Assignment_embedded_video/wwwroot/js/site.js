e23fa884240819e6180252d880072f195e20689e
Gil Furman <gilfurman88@gmail.com>
2022-07-12 11:51:45 -0400

    Add project files.

---
+++ Assignment_embedded_video/wwwroot/js/site.js
@@ -0,0 +1,14 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+
+
+// Functions to called onmouseover and on mouseout to change the color of "video_title"
+function newColor() {
+    document.getElementById("title").style.color = "gold";
+}
+
+function oldColor() {
+    document.getElementById("title").style.color = "goldenrod";
+}
